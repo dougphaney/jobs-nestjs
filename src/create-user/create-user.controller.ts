81cbae4de3d020c11e31f1d035d36876282fe6fb
dougphaney <dougphaney@hotmail.com>
2021-06-13 13:00:52 -0300

    Create Post Method

--- src/create-user/create-user.controller.ts
+++ src/create-user/create-user.controller.ts
@@ -1,9 +1,11 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { CreateUserDTO } from './create-user-dto';
 
 @Controller('create-user')
 export class CreateUserController {
-  @Get('/')
-  createUser() {
-    return 'Hello Word!';
+  @Post('/')
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  createUser(@Body() createUser: CreateUserDTO) {
+    return createUser;
   }
 }
