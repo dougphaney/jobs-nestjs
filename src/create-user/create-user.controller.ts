f87d16ee411898a504a2bdd58321e83eeeeb0fc0
dougphaney <dougphaney@hotmail.com>
2021-06-13 13:36:00 -0300

    create faker mail with ethereal email

--- src/create-user/create-user.controller.ts
+++ src/create-user/create-user.controller.ts
@@ -1,11 +1,19 @@
 import { Body, Controller, Post } from '@nestjs/common';
+import { MailerService } from '@nestjs-modules/mailer';
 import { CreateUserDTO } from './create-user-dto';
 
 @Controller('create-user')
 export class CreateUserController {
+  constructor(private mailService: MailerService) {}
   @Post('/')
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  createUser(@Body() createUser: CreateUserDTO) {
+  async createUser(@Body() createUser: CreateUserDTO) {
+    await this.mailService.sendMail({
+      to: createUser.email,
+      from: 'dougphaney team',
+      subject: 'welcome!!',
+      text: `Hello ${createUser.name}, welcome to join this group`,
+    });
     return createUser;
   }
 }
